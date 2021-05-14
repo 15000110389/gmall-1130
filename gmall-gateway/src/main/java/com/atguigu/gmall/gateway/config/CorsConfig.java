e481ab1cfa5e9e8d4009c047ffd19b78a03ace24
Atguigu <1445534852@qq.com>
2021-05-14 13:58:37 +0800

    网关

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.gmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://manager.gmall.com");
+        corsConfiguration.addAllowedOrigin("http://localhost:1000");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
+    }
+}
---
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/mapper/WareSkuMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.wms.mapper;
+
+import com.atguigu.gmall.wms.entity.WareSkuEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品库存
+ * 
+ * @author fenggew
+ * @email fengge@atguigu.com
+ * @date 2021-05-13 20:37:10
+ */
+@Mapper
+public interface WareSkuMapper extends BaseMapper<WareSkuEntity> {
+	
+}
